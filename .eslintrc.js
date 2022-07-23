6fc507a0a6684e0deb6f02e3f74b0549802a1ea7
luthfan <luthfan.a.haq@gmail.com>
2022-07-23 11:36:51 +0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+export default {
+    "env": {
+        "commonjs": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "airbnb-base"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "no-console": "off"
+    }
+}
